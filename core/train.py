2962e30346c69ae1989388b6e81f8be2232aaa35
Anurag Koul <koulanurag@gmail.com>
2020-05-21 14:27:31 -0700

    corrected logging of critic_2 loss

--- core/train.py
+++ core/train.py
@@ -204,7 +204,7 @@ def train(config: BaseConfig, writer: SummaryWriter):
 
                 # Log
                 writer.add_scalar('train/critic_1_loss', critic_1_loss, total_env_steps)
-                writer.add_scalar('train/critic_2_loss', critic_1_loss, total_env_steps)
+                writer.add_scalar('train/critic_2_loss', critic_2_loss, total_env_steps)
                 writer.add_scalar('train/policy_loss', policy_loss, total_env_steps)
                 # print(round(critic_1_loss, 2), round(critic_2_loss, 2), round(policy_loss, 2))
 
