987050a9c5c328dd72b1b9dda030ac32075c75d1
Anurag Koul <koulanurag@gmail.com>
2020-05-22 23:45:55 -0700

    buffer size match up with original td3 code

--- config/mujoco/__init__.py
+++ config/mujoco/__init__.py
@@ -9,7 +9,7 @@ class MujocoConfig(BaseConfig):
         super(MujocoConfig, self).__init__(max_env_steps=int(1e6),
                                            start_step=int(25e3),
                                            lr=3e-4,
-                                           replay_memory_capacity=100000,
+                                           replay_memory_capacity=int(1e6),
                                            fixed_action_repeat=1)
 
     def new_game(self, seed=None, save_video=False, video_dir_path=None, uid=None):
